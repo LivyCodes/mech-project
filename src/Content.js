67cf17f8553d915e03f076405414cf1794664cfa
Olive <njerio86@gmail.com>
2023-04-14 20:51:55 +0300

    added the mech projects file and added content to the navbar

---
+++ src/Content.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Content.css";
+
+function ContentBody() {
+  return (
+    <div>
+      <h1>BIRECH MOBILE MECHANIC</h1>
+    </div>
+  );
+}
+
+export default ContentBody;
---
+++ src/Navbar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./Navbar.css";
+
+function LeftNavBar() {
+  return (
+    <div className="left-nav">
+      <Navbar bg="light" expand="sm" className="justify-content-start">
+        <Container>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home" className="custom-nav-link">
+                HOME
+              </Nav.Link>
+              <Nav.Link href="#link" className="custom-nav-link">
+                A/C SERVICES
+              </Nav.Link>
+              <Nav.Link href="#link" className="custom-nav-link">
+                AUTO MAINTENANCE
+              </Nav.Link>
+              <Nav.Link href="#link" className="custom-nav-link">
+                BRAKES
+              </Nav.Link>
+              <Nav.Link href="#link" className="custom-nav-link">
+                TRANSMISSIONS
+              </Nav.Link>
+              <Nav.Link href="#link" className="custom-nav-link">
+                FLOAT SERVICES
+              </Nav.Link>
+              <Nav.Link href="#link" className="custom-nav-link">
+                EXHAUST & MUFFLERS
+              </Nav.Link>
+              <Nav.Link href="#link" className="custom-nav-link">
+                OIL CHANGES
+              </Nav.Link>
+              <Nav.Link href="#link" className="custom-nav-link">
+                STRUTS
+              </Nav.Link>
+              <Nav.Link href="#link" className="custom-nav-link">
+                ABOUT US
+              </Nav.Link>
+              <Nav.Link href="#link" className="custom-nav-link">
+                CONTACT US
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default LeftNavBar;
